02c8916a3056c07becf29969ab3b3cb729957770
Jan Vornberger <jan@271.de>
2020-03-12 21:11:55 +0100

    Add usage note

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -24,6 +25,11 @@ func installSignalHandlers(cancel context.CancelFunc) {
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s <socket path>\n", os.Args[0])
+		os.Exit(1)
+	}
+
 	ctx, cancel := context.WithCancel(context.Background())
 	go installSignalHandlers(cancel)
 
@@ -31,7 +37,7 @@ func main() {
 	go broadcaster.Serve(ctx, broadcasterAddress)
 
 	skygazer := skygazer.New(broadcaster)
-	err := skygazer.Listen(ctx, "skygaze.sock")
+	err := skygazer.Listen(ctx, os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
